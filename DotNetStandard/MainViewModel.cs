788110b0f844140444a0ebb04f3306268782eca9
Laurent Bugnion <lbugnion@outlook.com>
2018-09-12 12:56:07 +0200

    Updated Functions URL in MainViewModel.
    Made sure that all UWP apps are getting deployed.

--- DotNetStandard/MainViewModel.cs
+++ DotNetStandard/MainViewModel.cs
@@ -7,7 +7,8 @@ namespace SharedFiles.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        private const string Url = "https://lbxamnamesample.azurewebsites.net/api/hello/name/{name}";
+        // private const string Url = "https://lbxamnamesample.azurewebsites.net/api/hello/name/{name}";
+        private const string Url = "https://lbsayhello.azurewebsites.net/api/hello/name/{name}";
 
         private RelayCommand<string> _executeCommand;
         private string _name = string.Empty;
